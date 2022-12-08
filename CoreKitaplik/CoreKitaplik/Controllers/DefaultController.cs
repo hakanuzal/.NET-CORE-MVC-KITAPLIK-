dd81ae3fe7ab9ae6319542657f29f478afae441b
hakanuzal <hakan224030@gmail.com>
2022-12-08 15:57:48 +0300

    Kitaplık Örneği
    Basit bir kitaplık örneği

---
+++ CoreKitaplik/CoreKitaplik/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreKitaplik.Controllers;
+using CoreKitaplik.Models;
+
+namespace CoreKitaplik.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ktp = new List<Kitaplar>()
+            {
+                new Kitaplar(){ ID = 1 , Name = "Şeker Portakalı", Author = "Jose Mauro"},
+                new Kitaplar(){ ID = 2 , Name = "Olasılıksız", Author = "Adam Fawer"},
+                new Kitaplar(){ ID = 3 , Name = "Sefiller", Author = "Victor Hugo"},
+                new Kitaplar(){ ID = 4 , Name = "Yeraltından Notlar", Author = "Dostoyevski"},
+                new Kitaplar(){ ID = 5 , Name = "Gazap Üzümleri", Author = "Jhon Steinbeck"}
+            };
+            return View(ktp);
+        }
+    }
+}
---
+++ CoreKitaplik/CoreKitaplik/Models/Kitaplar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreKitaplik.Models
+{
+    public class Kitaplar
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
